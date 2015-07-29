7cb70052012b5bb6cc35ca9174c3e4a268064bda
muffl0n <sven@schliesing.de>
2015-07-29 08:40:12 +0200

    removed example for deprecated argument sha256sum

--- network/basics/get_url.py
+++ network/basics/get_url.py
@@ -149,9 +149,6 @@ EXAMPLES='''
 - name: download foo.conf
   get_url: url=http://example.com/path/file.conf dest=/etc/foo.conf mode=0440
 
-- name: download file with sha256 check
-  get_url: url=http://example.com/path/file.conf dest=/etc/foo.conf sha256sum=b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c
-
 - name: download file and force basic auth
   get_url: url=http://example.com/path/file.conf dest=/etc/foo.conf force_basic_auth=yes
 
